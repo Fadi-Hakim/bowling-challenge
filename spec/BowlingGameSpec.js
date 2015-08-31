9cc793e4b94c923f18f8d51aef29ec929e6e67f8
Fadi-Hakim <Fadihakim@msn.com>
2015-09-01 00:36:18 +0100

    Added score and roll methods

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,25 @@
+describe('Bowling Game', function(){
+
+  var game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
+  });
+
+  it('can roll a gutter game', function() {
+    var game = new BowlingGame();
+    for (var i=0; i<20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
+  it('can roll all ones', function() {
+    var game = new BowlingGame();
+    for (var i=0; i<20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
+  });
+
+});
